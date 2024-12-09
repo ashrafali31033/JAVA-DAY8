249278b0730b71942d263b8f3049cce4680567be
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-09 10:46:43 +0530

    Add files via upload

---
+++ arr1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class post
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int ns[]=new int[n];
+        int k=0,s=0;
+        
+        
+        for(int i=1;i<n;i++)
+        {
+            s+=i;
+            ns[k]=s;
+            k++;
+        }
+        
+          
+        
+        System.out.println(ns[n/2]);
+              
+        
+    }
+}
